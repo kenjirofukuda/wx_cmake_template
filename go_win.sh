510fa46c450fd82f82d2883e2d82d87e07a6076d
kenjirofukuda <kenjirofukuda@gmail.com>
2023-04-10 21:39:43 +0900

    add library html xml xrc

--- go_win.sh
+++ go_win.sh
@@ -3,13 +3,13 @@
 top_dir=$(dirname $0)
 cd "$top_dir"
 entry_point=main
-cmake -S . -B build 
+cmake -S . -B build -G "Visual Studio 16 2019" -A x64 
 if [ $? -ne 0 ]; then
     echo "CMake Config fail"
     exit 1
 fi
 
-cmake --build build 
+cmake --build build
 if [ $? -ne 0 ]; then
     echo "CMake Build fail"
     exit 1
